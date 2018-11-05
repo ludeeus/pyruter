dc44222495207de75f3cbb64b24b9528c4a914ab
ludeeus <joasoe@gmail.com>
2018-11-05 20:46:21 +0100

    added datautil to setup

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.md", "r") as fh:
     LONG = fh.read()
 setuptools.setup(
     name="pyruter",
-    version="1.0.1",
+    version="1.0.2",
     author="Joakim Sorensen",
     author_email="ludeeus@gmail.com",
     description="",
     long_description=LONG,
-    install_requires=['aiohttp', 'async_timeout'],
+    install_requires=['aiohttp', 'async_timeout', 'dateutil'],
     long_description_content_type="text/markdown",
     url="https://github.com/ludeeus/pyruter",
     packages=setuptools.find_packages(),
