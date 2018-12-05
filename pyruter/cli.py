415d8b9c8bfd48caa82c1a1201bfd3beb670a117
Joakim Sørensen <joasoe@gmail.com>
2018-12-05 18:03:27 +0100

    Destinations (#5)
    * adds destinations
    
    * added dest
    
    * typo
    
    * linting

--- pyruter/cli.py
+++ pyruter/cli.py
@@ -8,7 +8,6 @@ import aiohttp
 @click.group()
 def commands():
     """Click group."""
-    pass
 
 
 @commands.command()
@@ -27,6 +26,23 @@ def departure(stop, destination):
             await data.get_departures()
             print(json.dumps(data.departures, indent=4, sort_keys=True))
     LOOP.run_until_complete(get_departures())
+
+
+@commands.command()
+@click.option('--stop', '-S', type=int, default=None, required=1,
+              help="Stop ID of the stop you want information from.")
+def destinations(stop):
+    """Get destination information."""
+    from pyruter.api import Departures
+
+    async def get_destinations():
+        """Get departure information."""
+        async with aiohttp.ClientSession() as session:
+            data = Departures(LOOP, stop, session=session)
+            result = await data.get_final_destination()
+            print(json.dumps(result, indent=4, sort_keys=True,
+                             ensure_ascii=False))
+    LOOP.run_until_complete(get_destinations())
 
 
 LOOP = asyncio.get_event_loop()
